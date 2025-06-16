bb42aa188001591ee21cffabcc924a9bdfe1f38e
Kumegawa-hitoshi <murilokumegawa@gmail.com>
2025-06-16 14:21:28 -0300

    Adiciona rodapé com informações de direitos autorais e link social

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+// footer
+?>
+</div> <footer>
+    <p>© <?php echo date("Y"); ?> Catálogo de Filmes. Todos os direitos reservados.</p>
+    <div style="margin-top: 10px;">
+        <a href="https://github.com/Kumegawa-hitoshi" target="_blank" class="text-dark me-3" style="font-size: 1.5rem; vertical-align: middle;">
+            <i class="bi bi-github"></i>
+        </a>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+</html>
